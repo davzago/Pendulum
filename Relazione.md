32dfb15e5448f9a81a154c3782378fe17cdb5f2f
Davide <zago-7@live.it>
2021-03-25 16:12:11 +0100

    finish

--- Relazione.md
+++ Relazione.md
@@ -1,26 +1,44 @@
 # Q-Learning con reti neurali
 
-Il Q-learning è una procedura per far imparare ad un'agente la migliore azione da compiere in un determinato stato dell'ambiente in cui si trova, questo normalmente viene fatto riempendo una tabella contenente un Q-value per ogni transizione tra stati.
+Il Q-learning è una procedura che ha come obbiettivo far imparare ad un'agente la migliore azione da compiere in un determinato stato dell'ambiente in cui si trova, questo normalmente viene fatto riempendo una tabella contenente un Q-value per ogni transizione tra stati.
 La regola utilizzata per aggiornare il Q value è: 
 $$
 Q(s,a) = r + \gamma max_{a'}Q(s',a')
 $$
 dove $r$ è il reward determinato dall'azione compiuta e $\gamma$ è il fattore di discount che decide l'importanza delle azioni successivo sul Q-value attuale. 
-L'agente esplorando l'ambiente e utilizzando questa regola riempirà la tabella fino a convergere alla effettiva funzione $Q^*$.
+L'agente esplorando l'ambiente e utilizzando questa regola riempirà la tabella fino a convergere alla migliore funzione $Q^*$.
 Una policy è una "regola" che l'agente segue per decidere che azioni compiere nel suo ambiente, nel caso del Q-Learning la policy che l'agente segue è quella di scegliere l'azione con corrispondente Q-value maggiore.
-Tuttavia questo metodo non può essere applicato se gli stati raggiungibili sono non numerabili, per ovviare a questo problema si può provare ad usare una rete neurale per cercare di approssimare la funzione Q*.
+Tuttavia questo metodo non può essere applicato se gli stati raggiungibili sono non numerabili, per ovviare a questo problema si può provare ad usare una rete neurale per cercare di approssimare la funzione Q*. Il limite di questo metodo è che il numero di azioni che si possono compiere in ogni stato deve essere finito.
 
-## DDQN
+## Ambiente 
 
-## LOSS
+L'ambiente in esame è **Pendulum-v0** di gym, in questo ambiente l'obbiettivo è far stare in piedi un pendolo spingendolo a destra e a sinistra con forza variabile. Il reward dato a ogni time step è una funzione dell'angolo del pendolo e della spinta data al pendolo e il suo valore massimo è 0, questo accade quando il pendolo è in verticale e non viene applicata nessuna forza.
+Per poter utilizzare una rete neurale in questo caso è stato necessario discretizzare il le azioni disponibili in quanto il numero di azioni deve essere pari al numero di neuroni di output della rete, quindi l'azione che poteva essere scelta in un intervallo continuo [-2,2] diventa una scelta tra [-2, 1, 0, 1, 2] per un totale di 5 possibili azioni per ogni stato.  
 
-## Come effettivamente viene fatto il training
+## DQN
+
+Una Deep Q Network è una rete che prende in input lo stato e l'azione compiuta e restituisce il Q-value corrispondente, lo stato è definito dall'oggetto chiamato observation e contiene 3 valori: il seno e il coseno dell'angolo del pendolo e la velocità angolare corrente.
+La rete dunque è costituita da un input layer di 4 nodi (observation + azione) seguito da 2 hidden layers di 100 e 50 nodi rispettivamente con *ReLu* come funzione di attivazione e infine un output layer con 5 nodi (1 per ogni azione) con attivazione lineare.
+
+## Loss Function
+
+Per effettuare il Training di questa rete c'è bisogno di una loss function, visto che stiamo applicando il Q-Learning vogliamo che l'output della rete diventi un Q-value, per ottenere questo risultato si utilizza:
+$$
+L(\theta)=E[(r + \gamma max_{a'} Q(s',a';\theta)- Q(s,a;\theta))^2]
+$$
+dove $Q(s,a;\theta)$ è il valore stimato dalla rete e i valori (s,a,s',a') sono estratti dal replay buffer (nostro training set), il valore di $\gamma$ in questo caso è $0.9$, in questo modo gli stati e le azioni future hanno un peso abbastanza alto ma non hanno lo stesso peso dello stato attuale.
+
+Una cosa da notare è che per calcolare la loss function c'è bisogno di utilizzare 2 time step, inoltre la rete viene utilizzata sia per stimare il Q-value sia per stimare il valore del target, il che è un problema in quanto facciamo un update della rete anche il target cambia. Nella pratica questo non portava buoni risultati, per questo motivo ho utilizzato un'altra rete (target) strutturalmente identica a quella principale ma che viene aggiornata ogni 20 training steps, questo fa si che il target non cambi in continuazione e rende il training più stabile.  
 
 ## Replay buffer
 
-### policy per la collezione
+Il replay buffer è il training set che verrà usato per allenare la DQN, visto che la rete ha bisogno di due time step per calcolare la loss a ogni train step alla rete viene fornito un Tensore di forma [64, 2, 4] dove 64 è la dimensione del batch, 2 è il numero di time step che servono a calcolare la loss function e 4 è la dimensione di un input.
+La collezione dei dati viene effettuata sia prima che in contemporanea al training, inizialmente vengono collezionati una serie di esempi con azioni casuali, dopo ogni training step vengono collezionati dei dati aggiuntivi utilizzando la policy di collezione dell'agente che in pratica utilizza i q-value forniti dalla rete ma con una certa probabilità $\epsilon = 0.1$ compie una azione casuale, questo permette all'agente di esplorare l'ambiente e trovare stati e azioni migliori.
 
-## Problemi e (possibili) soluzioni
+## Risultati
 
-## Risultati con (avg return)
+Per dare un valore ai risultati ogni 1000 training step l'agente è stato valutato in 10 episodi di 200 time step ciascuno, da questi episodi si ottiene il reward medio, il miglore risultato è -254 ottenuto dopo la fine della fase di training.
 
+<img src="/home/davide/Documenti/Pendulum/avg_return_graph.jpg" alt="avg_return" style="zoom:67%;" />
+
+Dopo il training l'agente risulta in grado di mettere in verticale il pendolo molto velocemente e di mantenerlo nella stessa posizione, il reward medio sarà comunque sempre negativo in quanto il massimo reward è 0 e compiere azioni porta reward negativi.
--- pendulum.py
+++ pendulum.py
@@ -2,6 +2,8 @@ import gym
 import numpy as np
 import tensorflow as tf
 import imageio
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 from tf_agents.specs import BoundedArraySpec
@@ -18,60 +20,34 @@ from tf_agents.trajectories import trajectory
 from tensorflow.data import DatasetSpec
 from tf_agents.drivers import dynamic_step_driver
 from tf_agents.trajectories.time_step import TimeStep
+from tf_agents.networks.q_network import QNetwork
 
 
-num_iterations = 8000 # @param {type:"integer"}
+num_iterations = 8000 
 
-initial_collect_steps = 100  # @param {type:"integer"} 
-collect_steps_per_iteration = 1  # @param {type:"integer"}
-replay_buffer_max_length = 100000  # @param {type:"integer"}
+initial_collect_steps = 100   
+collect_steps_per_iteration = 1  
+replay_buffer_max_length = 100000  
 
-batch_size = 64  # @param {type:"integer"}
-learning_rate = 1e-3  # @param {type:"number"}
-log_interval = 200  # @param {type:"integer"}
+batch_size = 64  
+learning_rate = 1e-3  
+log_interval = 200  
 
-num_eval_episodes = 10  # @param {type:"integer"}
-eval_interval = 1000  # @param {type:"integer"}
+num_eval_episodes = 10  
+eval_interval = 1000  
 
 env_name = 'Pendulum-v0'
 num_actions = 5
-
-"""envlol = gym.make(env_name)
-dims = envlol.action_space.shape[0]
-print(dims)"""
-
-"""env = wrappers.ActionDiscretizeWrapper(suite_gym.load(env_name), num_actions=9)
-#print('Discretized Action Spec:', env.action_spec())
-env.reset()
-observations = []
-rewards = []
-done = []
-infos = []
-initial_state = env.reset()
-observations.append(initial_state)
-action = [8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,4,4,4,4,4] #0,1,2,3,4,5,6,7,8
-for i in range(20):
-    env.render() 
-    #action = 0 # env.action_space.sample()
-    (obs, reward, d, info) = env.step(action[i]) # take a random action
-    observations.append(obs)
-    rewards.append(reward)
-    done.append(d)
-    infos.append(info)
-
-time_step = env.reset()"""
 
 train_py_env = wrappers.ActionDiscretizeWrapper(suite_gym.load(env_name), num_actions=num_actions)
 eval_py_env = wrappers.ActionDiscretizeWrapper(suite_gym.load(env_name), num_actions=num_actions)
 train_env = tf_py_environment.TFPyEnvironment(train_py_env)
 eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
-#print("action space:", env.action_spec())
 
 "observation specification: BoundedArraySpec(shape=(3,), dtype=dtype('float32'), name='observation', minimum=[-1. -1. -8.], maximum=[1. 1. 8.])"
 "reward specification: ArraySpec(shape=(), dtype=dtype('float32'), name='reward')"
 
 fc_layer_params = (100, 50)
-#print("Num actions:", num_actions)
 
 def dense_layer(num_units):
   return tf.keras.layers.Dense(
@@ -89,13 +65,12 @@ q_values_layer = tf.keras.layers.Dense(
     bias_initializer=tf.keras.initializers.Constant(-0.2))
 q_net = sequential.Sequential(dense_layers + [q_values_layer])
 
-#print(q_net.summary())
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
 train_step_counter = tf.Variable(0)
 
-agent = dqn_agent.DdqnAgent(
+agent = dqn_agent.DqnAgent(
     train_env.time_step_spec(),
     train_env.action_spec(),
     q_network=q_net,
@@ -136,7 +111,6 @@ replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
     batch_size = train_env.batch_size,
     max_length = replay_buffer_max_length 
 )
-print("train_env.batch_size:", train_env.batch_size)
 
 replay_observer = [replay_buffer.add_batch]
 
@@ -147,64 +121,35 @@ collect_op = dynamic_step_driver.DynamicStepDriver(
   observers=replay_observer,
   num_steps=initial_collect_steps)
 
-"""def collect_step(environment, policy, buffer):
-  time_step = environment.current_time_step()
-  action_step = policy.action(time_step)
-  next_time_step = environment.step(action_step.action)
-  traj = trajectory.from_transition(time_step, action_step, next_time_step)
-
-  # Add trajectory to the replay buffer
-  buffer.add_batch(traj)
-
-def collect_data(env, policy, buffer, steps):
-  for _ in range(steps): 
-    collect_step(env, policy, buffer) 
-
-collect_data(train_env, random_policy, replay_buffer, initial_collect_steps)
-
-dataset = replay_buffer.as_dataset(
-    num_parallel_calls=3,                                                                                                                                   
-    sample_batch_size=batch_size, 
-    num_steps=2).prefetch(3)"""
-
 agent.train_step_counter.assign(0)
 
 avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
 returns = [avg_return]
                     
-collect_op.run()#time_step=time_step)
-
-print(" \n single trajectories:", iter(replay_buffer.as_dataset()).next())
+collect_op.run()
 
 dataset = replay_buffer.as_dataset(
   num_parallel_calls=3,                                                                                                                                   
   sample_batch_size=batch_size, 
   num_steps=2).prefetch(3)
 
-
-iterator = iter(dataset)  
-print("\n grouped trajectories:", iter(iterator.next()[0]))
-                                                                                                   
+iterator = iter(dataset)                                                                                                     
 
 for _ in range(num_iterations):
-  # Collect a few steps using collect_policy and save to the replay buffer.
-  #collect_data(train_env, agent.collect_policy, replay_buffer, collect_steps_per_iteration)
   collect_op.run(maximum_iterations=collect_steps_per_iteration)
 
-  # Sample a batch of data from the buffer and update the agent's network.
   experience, unused_info = next(iterator)
   train_loss = agent.train(experience).loss
 
   step = agent.train_step_counter.numpy()
-  print(step)
 
-  """if step % log_interval == 0:
+  if step % log_interval == 0:
     print('step = {0}: loss = {1}'.format(step, train_loss))
 
   if step % eval_interval == 0:
     avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
     print('step = {0}: Average Return = {1}'.format(step, avg_return))
-    returns.append(avg_return)"""
+    returns.append(avg_return)
 
 
 def create_policy_eval_video(policy, filename, num_episodes=5, fps=30):
@@ -219,4 +164,12 @@ def create_policy_eval_video(policy, filename, num_episodes=5, fps=30):
         video.append_data(eval_py_env.render())
   #return embed_mp4(filename)
 
+iterations = range(0, num_iterations + 1, eval_interval)
+plt.plot(iterations, returns)
+plt.ylabel('Average Return')
+plt.xlabel('Iterations')
+plt.ylim(top=250)
+plt.savefig('avg_return.jpg')
+
 create_policy_eval_video(agent.policy, "eval_video", 1)
+
